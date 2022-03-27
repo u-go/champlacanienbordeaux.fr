aeffeb94c820250a63ce77bd853a076b28af2bb6
Ugo <grandinetti.ugo@gmail.com>
2022-03-28 00:38:00 +0200

    ui

--- footer.php
+++ footer.php
@@ -8,10 +8,9 @@
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo("template_directory"); ?>/style.css" />
 <!-- SCRIPT -->
-
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" assync defer></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" crossorigin="anonymous" assync defer></script>
-
 </body>
 </html>
