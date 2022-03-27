47b9c5dc71841356438585f4e142b4420347965e
Ugo <grandinetti.ugo@gmail.com>
2022-03-28 00:12:10 +0200

    remove ACF fields

--- index.php
+++ index.php
@@ -5,13 +5,9 @@
 	<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
 	<article class="card mb-3">
 		<div class="row g-0">
-			<div class="col-md-4">
-				<img src="<?php the_field('image'); ?>" class="img-fluid rounded-start" height="100%" alt="<?php the_field('description'); ?>">
-			</div>
 			<div class="col-md-8">
 				<div class="card-body">
 					<h2 class="card-title"><?php the_title() ?></h2>
-					<p class="card-text"><?php the_field('description'); ?></p>
 					<p class="card-text"><small><a href="<?php the_permalink(); ?>">Lire l'article</a></small></p>
 				</div>
 			</div>
