b5adb8e0adf9b0941c062cb8b1ae843df7bd2377
Ugo <grandinetti.ugo@gmail.com>
2022-03-28 00:32:48 +0200

    update

--- header.php
+++ header.php
@@ -17,29 +17,40 @@ if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandl
 </head>
 <body class="container-fluid">
 <header class="row">
-<div class="col-md-12">
-  <div class="display-5">Champs lacanien Bordeaux</div>
-</div>
-<div class="col-md-12">
-  <nav class="navbar navbar-expand-md navbar-light bg-light mt-3 mb-3" role="navigation">
-    <div class="container">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
-          <span class="navbar-toggler-icon"></span>
-      </button>
-          <?php
-          wp_nav_menu([
-            'theme_location'  => 'top',
-            'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
-            'container'       => 'div',
-            'container_class' => 'collapse navbar-collapse',
-            'container_id'    => 'bs-example-navbar-collapse-1',
-            'menu_class'      => 'navbar-nav mr-auto',
-            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-            'walker' => new WP_Bootstrap_Navwalker()
-          ]);
-          ?>
-      </div>
-  </nav>
-</div>
+  <div class="row flex-nowrap justify-content-between align-items-center">
+    <div class="col-4 pt-1">
+      <a class="link-secondary" href="#">Subscribe</a>
+    </div>
+    <div class="col-4 text-center">
+      <a class="blog-header-logo text-dark" href="#">Champs lacanien Bordeaux</a>
+    </div>
+    <div class="col-4 d-flex justify-content-end align-items-center">
+      <a class="link-secondary" href="#" aria-label="Search">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><path d="M21 21l-5.2-5.2"></path></svg>
+      </a>
+      <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+    </div>
+  </div>
+  <div class="col-md-12">
+    <nav class="navbar navbar-expand-md navbar-light bg-light mt-3 mb-3" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <?php
+            wp_nav_menu([
+              'theme_location'  => 'top',
+              'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+              'container'       => 'div',
+              'container_class' => 'collapse navbar-collapse',
+              'container_id'    => 'bs-example-navbar-collapse-1',
+              'menu_class'      => 'navbar-nav mr-auto',
+              'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+              'walker' => new WP_Bootstrap_Navwalker()
+            ]);
+            ?>
+        </div>
+    </nav>
+  </div>
 </header>
